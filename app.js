4da0c2e5d6346a07fc032b909982306444bfad1f
Paulo Ricardo Silva dos Santos Filho <desenvolvimento4@codi.com.br>
2023-05-22 17:07:20 -0300

    "ADICIONANDO AUDIO"

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 
 let botoes = document.querySelectorAll('.drum');
 for (let index = 0; index < botoes.length; index++) {
-    botoes[index].addEventListener('click', handleClick)   
+    botoes[index].addEventListener('click', handleClick)
+    switch (botoes[index].className) {
+        case 'w drum':
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play
+            break;
+    
+        default:
+            break;
+    }   
 }
 
 
