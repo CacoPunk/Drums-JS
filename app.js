69de9e07915cc4d926ae9db1dd1ca17dc06f0a5d
Paulo Ricardo <pauloricardossfilho@gmail.com>
2023-05-22 21:42:44 -0300

    Adicionado imagem e som aos botoes

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 
 let botoes = document.querySelectorAll('.drum');
 for (let index = 0; index < botoes.length; index++) {
-    botoes[index].addEventListener('click', handleClick)
-    switch (botoes[index].className) {
-        case 'w drum':
-            var audio = new Audio('sounds/crash.mp3');
-            audio.play
-            break;
+    botoes[index].addEventListener('click', function(){
+        this.style.color = '#ffff'
+        switch (this.innerHTML) {
+            case 'w':
+                let audiow = new Audio('./sounds/tom-1.mp3')
+                audiow.play();
+                break;
+            case 'a':
+                let audioa = new Audio('./sounds/tom-2.mp3')
+                audioa.play();
+                break;
+            case 's':
+                let audios = new Audio('./sounds/tom-3.mp3')
+                audios.play();
+                break;
+            case 'd':
+                let audiod = new Audio('./sounds/tom-4.mp3')
+                audiod.play();
+                break;
+            case 'j':
+                let audioj = new Audio('./sounds/snare.mp3')
+                audioj.play();
+                break;
+            case 'k':
+                let audiok = new Audio('./sounds/crash.mp3')
+                audiok.play();
+                break;  
+            case 'l':
+                let audiol = new Audio('./sounds/kick-bass.mp3')
+                audiol.play();
+                break;      
+            default:
+                break;
+        }
+        
+        
+        
+        
+    })
     
-        default:
-            break;
-    }   
-}
-
-
-
-function handleClick() {
-    alert("clicou")
 }
