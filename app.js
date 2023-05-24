8b1c8e147fb1769c56c88e48ec792fdcdb49e858
Paulo Ricardo Silva dos Santos Filho <desenvolvimento4@codi.com.br>
2023-05-24 12:53:59 -0300

    Projeto finalizado.

--- app.js
+++ app.js
@@ -2,45 +2,57 @@
 let botoes = document.querySelectorAll('.drum');
 for (let index = 0; index < botoes.length; index++) {
     botoes[index].addEventListener('click', function(){
-       makeSound(this.innerHTML);   
+        let key = this.innerHTML
+        makeSound(key);
+        buttonAnimation(key);   
      })   
 }
 
 document.addEventListener('keydown', function(event){
-    makeSound(event.key);
+    let key = event.key
+    makeSound(key);
+    buttonAnimation(key)
 })
 
 function makeSound(key) {
     switch (key) {
         case 'w':
-            let audiow = new Audio('./sounds/tom-1.mp3')
+            let audiow = new Audio('./sounds/tom-1.mp3');
             audiow.play();
             break;
         case 'a':
-            let audioa = new Audio('./sounds/tom-2.mp3')
+            let audioa = new Audio('./sounds/tom-2.mp3');
             audioa.play();
             break;
         case 's':
-            let audios = new Audio('./sounds/tom-3.mp3')
+            let audios = new Audio('./sounds/tom-3.mp3');
             audios.play();
             break;
         case 'd':
-            let audiod = new Audio('./sounds/tom-4.mp3')
+            let audiod = new Audio('./sounds/tom-4.mp3');
             audiod.play();
             break;
         case 'j':
-            let audioj = new Audio('./sounds/snare.mp3')
+            let audioj = new Audio('./sounds/snare.mp3');
             audioj.play();
             break;
         case 'k':
-            let audiok = new Audio('./sounds/crash.mp3')
+            let audiok = new Audio('./sounds/crash.mp3');
             audiok.play();
             break;  
         case 'l':
-            let audiol = new Audio('./sounds/kick-bass.mp3')
+            let audiol = new Audio('./sounds/kick-bass.mp3');
             audiol.play();
             break;      
         default:
             break;
     };
+}
+
+function buttonAnimation(currentKey){
+    let activeButton = document.querySelector('.' + currentKey);
+    activeButton.classList.add('pressed')
+    setTimeout(() => {
+        activeButton.classList.remove('pressed') 
+    }, 100);
 }
